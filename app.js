a121668fa0aab3159302ed0cb9fc8bd37d9956e1
donas9269 <dieudonneaziano67@gmail.com>
2026-03-31 12:41:36 +0000

    mise a jour

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+function display(){
+const name= document.getElementById('nom').value
+const firstName= document.getElementById('prenom').value
+document.getElementById('vu').innerHTML="Bienvenue "+name+" "+firstName
+
+
+}
+
+
+
